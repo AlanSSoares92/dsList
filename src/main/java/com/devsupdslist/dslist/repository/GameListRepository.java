18deb7b2057bc165d22fe651bf408d5d0dfe0a59
AlanSSoares92 <alan_s_soares@outlook.com>
2025-01-25 23:05:28 -0300

    Domain model

--- src/main/java/com/devsupdslist/dslist/repository/GameListRepository.java
+++ src/main/java/com/devsupdslist/dslist/repository/GameListRepository.java
@@ -1,9 +1,9 @@
 package com.devsupdslist.dslist.repository;
 
-import com.devsupdslist.dslist.entities.Game;
+import com.devsupdslist.dslist.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameRepository extends JpaRepository<Game, Long> {
+public interface GameListRepository extends JpaRepository<GameList, Long> {
 }
